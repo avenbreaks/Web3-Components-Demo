a45a94ecc575d00f93a51cdf01a6f02e317add3c
Harish Raisinghani <harishraisinghani@gmail.com>
2022-08-30 17:39:26 -0700

    Update app

--- src/App.js
+++ src/App.js
@@ -1,31 +1,99 @@
 import React, { useState } from "react";
-import { TokenBalances } from '@covalenthq/web3-components';
-import {
-  BrowserRouter as Router,
-  Link,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 import { Input, Menu } from 'antd';
+import './App.css';
+import { TokenBalances, ERC20Transfers, TokenHolders} from '@covalenthq/web3-components';
+
 const { Search } = Input;
 
+const FormControl = ({placeholder, onSearch}) => {
+  return (
+    <Search placeholder={placeholder} onSearch={onSearch}
+      style={{
+        width: 500,
+      }}
+    />
+  )
+}
 
 const items = [
   {
     label: (
-      <Link to="/">Web3 Wallet</Link>
+      <Link to="/">Token Balances</Link>
     ),
-    key: 'web3',
+    key: 'tokenBalances',
   },
   {
     label: (
-      <Link to="/web2">Web2 Wallet</Link>
+      <Link to="/erc20Transfers">ERC20 Transfers</Link>
     ),
-    key: 'web2'
+    key: 'erc20Transfers'
+  },
+  {
+    label: (
+      <Link to="/tokenHolders">Token Holders</Link>
+    ),
+    key: 'tokenHolders'
   }
 ]
 
-export default function App() {
-  const [current, setCurrent] = useState("web3")
+const SearchTokenBalances = () => {
+  const [address, setAddress] = useState(null)
+  const [chainId, setChainId] = useState(1)
+
+  const onSearch = (value) => setAddress(value);
+  
+  return(
+    <div className="App" style={{ width: "100vw", padding: "25px" }}>
+      <FormControl placeholder="Enter an Ethereum wallet address or ENS" onSearch={onSearch} />
+      <div>
+        <br></br>
+        <p><b>Provided Address:</b> {address}</p>
+        <p><b>Set Chain Id:</b> {chainId}</p>
+        <TokenBalances address={address} chainId={chainId} />
+      </div>
+    </div>
+  )
+}
+
+const SearchTokenHolders = () => {
+  const [tokenAddress, setTokenAddress] = useState(null)
+  const [chainId, setChainId] = useState(1)
+
+  const onSearch = (value) => setTokenAddress(value);
+  
+  return(
+    <div className="App" style={{ width: "100vw", padding: "25px" }}>
+      <FormControl placeholder="Enter an Ethereum token (ERC20 or NFT) contract address" onSearch={onSearch} />
+      <div>
+        <br></br>
+        <TokenHolders tokenAddress={tokenAddress} chainId={chainId} />
+      </div>
+    </div>
+  )
+}
+
+const SearchERC20Transfers = () => {
+  const [walletAddress, setWalletAddress] = useState(null)
+  const [chainId, setChainId] = useState(1)
+
+  const onSearch = (value) => setWalletAddress(value);
+  
+  return(
+    <div className="App" style={{ width: "100vw", padding: "25px" }}>
+      <FormControl placeholder="Enter an Ethereum wallet address or ENS" onSearch={onSearch} />
+      <div>
+        <br></br>
+        <p><b>Provided Address:</b> {walletAddress}</p>
+        <p><b>Set Chain Id:</b> {chainId}</p>
+        <ERC20Transfers address={walletAddress} chainId={chainId} />
+      </div>
+    </div>
+  )
+}
+
+function App() {
+  const [current, setCurrent] = useState('tokenBalances')
 
   const onClick = (e) => {
     setCurrent(e.key)
@@ -33,74 +101,14 @@ export default function App() {
   
   return (
     <Router>
-      <div>
         <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />
-        <Route exact path="/web2">
-          <Web2 />
-        </Route>
-        <Route exact path="/">
-          <MetaMask />
-        </Route>
-      </div>
+        <Routes>
+          <Route path="/" element={<SearchTokenBalances />} />
+          <Route path="/erc20Transfers" element={<SearchERC20Transfers />} />
+          <Route path="/tokenHolders" element={<SearchTokenHolders />} />
+        </Routes>
     </Router>
   );
 }
 
-function MetaMask() {
-  const [address, setAddress] = useState("")
-  const [chainId, setChainId] = useState("")
-  
-  if (window.ethereum) {
-    // res[0] for fetching a first wallet
-    window.ethereum
-      .request({ method: "eth_requestAccounts" })
-      .then((res) => setAddress(res[0]));
-
-    window.ethereum
-      .request({ method: "eth_chainId" })
-      .then((res) => setChainId(parseInt(res, 16)));
-  } else {
-    alert("Please install the MetaMask extension!!");
-  }
-
-  
-  return(
-    <div className="App" style={{ width: "100vw", padding: "25px" }}>
-      <div>
-        <br></br>
-        <p><b>Injected Web3 Address:</b> {address}</p>
-        <p><b>Injected Web3 Chain Id:</b> {chainId}</p>
-        <br></br>
-        <TokenBalances apikey="ckey_3bdabb7325ee47cc9bcc1e5f3d7" address={address} chainId={chainId} />
-      </div>
-      
-    </div>
-    
-  )
-}
-
-function Web2() {
-  const [address, setAddress] = useState("")
-  const [chainId, setChainId] = useState(1)
-
-  const onSearch = (value) => setAddress(value);
-  
-  return(
-    <div className="App" style={{ width: "100vw", padding: "25px" }}>
-      <Search
-        placeholder="Enter an Ethereum wallet address or ENS"
-        onSearch={onSearch}
-        style={{
-          width: 350,
-        }}
-      />
-      <div>
-        <br></br>
-        <p><b>Provided Address:</b> {address}</p>
-        <p><b>Set Chain Id:</b> {chainId}</p>
-        <TokenBalances apikey="ckey_3bdabb7325ee47cc9bcc1e5f3d7" address={address} chainId={chainId} />
-      </div>
-    </div>
-    
-  )
-}
+export default App;
